b484dab33ebad39a58869189f075497cfe96cecf
user1 <user1>
2021-02-03 19:09:32 +0700

    Initial commit

---
+++ src/y.kt
@@ -0,0 +1,32 @@
+fun main () {
+    var HP = 100
+    var damage = 50
+    var enemyHP = 50
+    var enemydamage = 20
+    while (HP > 0 && enemyHP > 0) {
+        println(" да/нет")
+        var p = readLine()
+        if (p == "да")
+            enemyHP -= damage
+       println( "нанести урон ? да / нет")
+        var o = readLine()
+
+        if (o == "да")
+            HP -= enemydamage
+        println(HP)
+        println(enemyHP)
+    }
+    if (HP == 0)
+        println(" ты умер ")
+    else println("я умер ")
+
+}
+
+
+
+
+
+
+
+
+
